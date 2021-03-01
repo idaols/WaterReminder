e0fc093f9fa31f0c3a241e99eec5a58c627cdaed
jennahakulinen <jenna.hakulinen@metropolia.fi>
2021-03-01 10:31:12 +0200

    Created a base for the main activity.

--- app/src/main/java/com/example/waterreminder/MainActivity.java
+++ app/src/main/java/com/example/waterreminder/MainActivity.java
@@ -3,18 +3,51 @@ package com.example.waterreminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+/**
+ * @author Jenna
+ * This main class implements the apps main screen, where can be added the amount of drunk water.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    //private Counter counterDrinkWater;
+
+    private TextView textViewWaterTotal;
+    private Number numberWater;
+    private Button buttonAddWater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //textViewWaterTotal = findViewById(R.id.counterDrinkWater);
+        //counterDrinkWater = new Counter();
+        //updateUI;
     }
 
-    int i = 0;
+    /**
+     * This is a method for the button. By pressing the button it will add the added number to
+     * total amount.
+     * @param view
+     */
 
-    int x = 1;
+    public void buttonAddWater(View view) {
+        //counterDrinkWater.plus();
+        //updateUI();
+    }
 
-    int y = 2;
+    /**
+     * This is a method for updating text view component.
+     */
+
+    private void updateUI() {
+        TextView textView = findViewById(R.id.textViewWaterTotal);
+        //textView.setText(Integer.toString(counterDrinkWater.getValue()));
+    }
+
+
+
 }
